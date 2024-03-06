2ed1851a43af5612c41890056a412db447391b41
rmeenakshisundara <162467795+rmeenakshisundara@users.noreply.github.com>
2024-03-06 21:28:51 +0530

    Add files via upload

---
+++ src/main/java/Sundaramjenkins/Add.java
@@ -0,0 +1,15 @@
+package Sundaramjenkins;
+
+public class Add {
+    static int a =2;
+   static int b=3;
+
+    public static void main(String[] args) {
+        if (a+b==5){
+            System.out.println("valid ok");
+        } else  {
+            System.out.println("not ok");
+        }
+    }
+
+}
